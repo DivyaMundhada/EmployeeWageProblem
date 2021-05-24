f62dcac7f3dcb2bb344131ed32a10fda1afd30e2
DivyaMundhada <divyamundada9@gmail.com>
2021-05-24 18:39:12 +0530

    Added Employee present or absent feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #! /bin/bash -x
 
 echo Welcome to shell script
+
+isEmployeePresent=$((RANDOM%2))
+
+if [ $isEmployeePresent -eq 1 ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
+
