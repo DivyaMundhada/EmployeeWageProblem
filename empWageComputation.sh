a96bdfe1e6df98706990f828d8cb405e13e0feaf
DivyaMundhada <divyamundada9@gmail.com>
2021-05-25 12:15:54 +0530

    Added Employee Wage Computation with a function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,18 +7,25 @@ IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
 totalworkinghrs=0
 day=0
+function getworkhrs() {
+        local empCheck=$1
+        local empHrs=0
+        case $empCheck in
+                $IS_PRESENT_FULL_TIME) empHrs=8 ;;
+                $IS_PRESENT_PART_TIME) empHrs=4 ;;
+                *) empHrs=0 ;;
+        esac
+        echo $empHrs
+}
+
 while [ $day -lt 20 ] & [ $totalworkinghrs -lt 80 ]
 do
-empCheck=$(( RANDOM % 3 ))
-case $empCheck in
-        $IS_PRESENT_FULL_TIME) empHrs=8 ;;
-        $IS_PRESENT_PART_TIME) empHrs=4 ;;
-        *) empHrs=0 ;;
-esac
-totalworkinghrs=$(( totalworkinghrs + empHrs ))
-salary=$(( empHrs * EMP_RATE_PER_HR ))
-echo "$salary"
-((day++))
+        empCheck=$(( RANDOM % 3 ))
+        empHrs="$( getworkhrs $empCheck )"
+        totalworkinghrs=$(( totalworkinghrs + empHrs ))
+        salary=$(( empHrs * EMP_RATE_PER_HR ))
+        echo "$salary"
+        ((day++))
 done
 totalsalary=$((totalworkinghrs*EMP_RATE_PER_HR))
 echo Total Salary:$totalsalary
