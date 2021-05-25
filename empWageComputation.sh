5103cce74daea9f6662c25f9cfa2cc0ed8d0ded7
DivyaMundhada <divyamundada9@gmail.com>
2021-05-25 11:54:25 +0530

    Added Employee Wage Computation for a Month Feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,15 +5,18 @@ echo Welcome to shell script
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
-
-empCheck=$(( RANDOM % 3))
-
+totalworkinghrs=0
+for ((i=0;i<=20;i++))
+do
+empCheck=$(( RANDOM % 3 ))
 case $empCheck in
         $IS_PRESENT_FULL_TIME) empHrs=8 ;;
         $IS_PRESENT_PART_TIME) empHrs=4 ;;
         *) empHrs=0 ;;
 esac
-
+totalworkinghrs=$(( totalworkinghrs + empHrs ))
 salary=$(( empHrs * EMP_RATE_PER_HR ))
-
-echo Salary of employee is $salary
+echo "$salary"
+done
+totalsalary=$((totalworkinghrs*EMP_RATE_PER_HR))
+echo Total Salary:$totalsalary
