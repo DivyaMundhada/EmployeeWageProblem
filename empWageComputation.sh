a20ffb65a9880dbff594abd4e7e84a2a73ada29f
DivyaMundhada <divyamundada9@gmail.com>
2021-05-25 11:36:23 +0530

    Added Part Time Employee and Wage Feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,18 +2,21 @@
 
 echo Welcome to shell script
 
-IS_PRESENT=1
-
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
 
-empCheck=$(( RANDOM % 2 ))
+empCheck=$(( RANDOM % 3))
 
-
-if [ $empCheck -eq $IS_PRESENT ]
+if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
 then
         empHrs=8
-        salary=$(( EMP_RATE_PER_HR * empHrs ))
+elif [ $empCheck -eq $IS_PRESENT_PART_TIME ]
+then
+        empHrs=4
 else
-        salary=0
+        empHrs=0
 fi
+salary=$(( empHrs * EMP_RATE_PER_HR ))
+
 echo Salary of employee is $salary
